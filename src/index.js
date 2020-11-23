8cedcbae702b0653959aaf40b89d49e668855919
hellomac87 <hellomac87@gmail.com>
2020-11-23 23:44:27 +0900

    subscription

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 const countModifier = (count = 0, action) => {
-    console.log(count, action);
     if(action.type === "ADD"){
         return count + 1;
     }else if( action.type === "MINUS"){
@@ -17,11 +16,19 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({type:"ADD"});
-countStore.dispatch({type:"ADD"});
-countStore.dispatch({type:"ADD"});
-countStore.dispatch({type:"ADD"});
+const onChange = () => {
+    number.innerText = countStore.getState();
+}
 
-countStore.dispatch({type:"MINUS"});
+countStore.subscribe(onChange);
 
-console.log(countStore.getState())
+const handleAdd = () => {
+    countStore.dispatch({type:"ADD"})
+}
+
+const handleMinus = () => {
+    countStore.dispatch({type:"MINUS"})
+}
+
+add.addEventListener('click', handleAdd)
+minus.addEventListener('click', handleMinus)
