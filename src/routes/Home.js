5f9393e61debdb64a6db46ca5adeffcf044967d7
hellomac87 <hellomac87@gmail.com>
2020-11-28 20:33:20 +0900

    connecting the store

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,26 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-function Home ()  {
-    const [text, setText] = useState("");
-    function onChange (e) {
-        setText(e.target.value);
-    }
-    function onSubmit(e){
-        e.preventDefault();
-        console.log(text);
-    }
-    return (
-        <>
-            <h1>
-                To Do
-            </h1>
-            <form onSubmit={onSubmit}>
-                <input type="text" value={text} onChange={onChange}/>
-                <button>add</button>
-            </form>
-            <ul></ul>
-        </>
-    )
+function Home() {
+  const [text, setText] = useState('');
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log(text);
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>add</button>
+      </form>
+      <ul></ul>
+    </>
+  );
 }
 
-export default Home
+export default Home;
